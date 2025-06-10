343bec8fbe2ea3ee1a15fc8afcdb8ed431a17334
pbgalta36 <110046510+pbgalta36@users.noreply.github.com>
2025-06-10 21:51:21 +0100

    chore: clear api key

--- agent/core.py
+++ agent/core.py
@@ -3,10 +3,13 @@ from langchain_openai import ChatOpenAI
 from agent.memory import get_memory
 import os
 
-openai_api_key=os.environ["OPENAI_API_KEY"]
+API_KEY = os.environ["OPENAI_API_KEY"]
 
 def create_agent():
     llm = ChatOpenAI(model="gpt-3.5-turbo",
-                     openai_api_key="openai_api_key" )
+                     openai_api_key= API_KEY,
+                     max_tokens=1000,
+                     temperature=0.5,
+                     top_p=0.9)
     memory = get_memory()
     return ConversationChain(llm=llm, memory=memory, verbose=True)
